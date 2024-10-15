7262b6af601d2aaa952276a6680b86870860e2fc
Void.ID <romaissaeabde@gmail.com>
2024-10-15 15:05:11 +0100

    ex00 done

--- Cpp Module 07/ex00/main.cpp
+++ Cpp Module 07/ex00/main.cpp
@@ -7,3 +7,65 @@ void  swap(T &a, T &b)
   a = b;
   b = c;
 }
+
+template <typename T>
+const T & min(const T &a, const T &b)
+{
+  if (a < b) return a;
+  return b;
+}
+
+template <typename T>
+const T & max(const T &a, const T &b)
+{
+  if (a > b) return a;
+  return b;
+}
+
+int main(void)
+{
+    // Test swap function
+    {
+        int a = 5;
+        int b = 10;
+        std::cout << "Before swap: a = " << a << ", b = " << b << std::endl;
+        swap(a, b);
+        std::cout << "After swap: a = " << a << ", b = " << b << std::endl;
+    }
+
+    // Test min function
+    {
+        int a = 5;
+        int b = 10;
+        std::cout << "Min of " << a << " and " << b << " is " << min(a, b) << std::endl;
+
+        // Edge case: equal values
+        a = 7;
+        b = 7;
+        std::cout << "Min of " << a << " and " << b << " is " << min(a, b) << std::endl;
+
+        // Test with floating-point numbers
+        float f1 = 3.14f;
+        float f2 = 2.71f;
+        std::cout << "Min of " << f1 << " and " << f2 << " is " << min(f1, f2) << std::endl;
+    }
+
+    // Test max function
+    {
+        int a = 5;
+        int b = 10;
+        std::cout << "Max of " << a << " and " << b << " is " << max(a, b) << std::endl;
+
+        // Edge case: equal values
+        a = 7;
+        b = 7;
+        std::cout << "Max of " << a << " and " << b << " is " << max(a, b) << std::endl;
+
+        // Test with double precision floating-point numbers
+        double d1 = 3.14159265359;
+        double d2 = 2.71828182846;
+        std::cout << "Max of " << d1 << " and " << d2 << " is " << max(d1, d2) << std::endl;
+    }
+
+    return 0;
+}
